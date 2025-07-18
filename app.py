8eff5d6c91d830aacb5831f0cc5a56556fe51f34
jainchirag21064-lab <jainchirag21064@gmail.com>
2025-07-18 12:00:24 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,31 @@
+
+from fastapi import FastAPI
+import requests
+
+
+app = FastAPI()
+
+
+headers = {
+    'X-Retain-Images': 'none',
+    'X-Return-Format': 'markdown'
+}
+
+@app.get("/health")
+async def read_root():
+    return {"Message": "Congrats! Proxy is up!"}
+
+@app.get("/scrape")
+async def read_root(url:str):
+    """
+    Fetch the page markdown using jina ai.
+    """    
+    jina_reader_api = "https://r.jina.ai/"
+    try:
+        response = requests.get(jina_reader_api+url, headers=headers)
+        if response.status_code != 200:
+            raise Exception(f"Failed to fetch page. Status code: {response.status_code}")            
+    except Exception as e:
+            print(f"An error occurred during scraping with playwright: {str(e)}")
+            return None            
+    return response.text
